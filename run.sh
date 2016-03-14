1a9a2bf30598804831b0cb5b17962a0809573953
Matt <binerf@mygaia.org>
2016-03-14 12:52:13 +0100

    first commit

---
+++ run.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -ex
+
+sed -i '/^SocksPort.*$/d' /etc/tor/torrc
+ip=`hostname -i`
+echo "SocksPort $ip:9050" >> /etc/tor/torrc
+
+sed -i '/^StrictNodes.*$/d' /etc/tor/torrc
+sed -i '/^ExitNodes.*$/d' /etc/tor/torrc
+
+if [ -n "$TOR_EXITNODES" ]; then
+  echo "StrictNodes 1" >> /etc/tor/torrc
+  echo "ExitNodes $TOR_EXITNODES" >> /etc/tor/torrc
+fi
+
+/usr/bin/tor -f /etc/tor/torrc 2>&1 &
+
+cd /root && python zeronet.py --ui_ip 0.0.0.0 --config_file /root/data/zeronet.conf
