c8b4e05a28bafb1be595cc77e841f6c82eb8fdff
Matt <binerf@mygaia.org>
2016-07-21 16:15:10 +0200

    Correction in dockerrun_zeronet_tor.sh

--- dockerrun_zeronet_tor.sh
+++ dockerrun_zeronet_tor.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-docker run -d -v /mnt/docker/zeronet/data:/home/zeronet/ZeroNet/data -p 15441:15441 -p 43110:43110 binerf/zeronet_tor
+docker run -d -v /mnt/docker/zeronet/data:/home/zeronet/ZeroNet/data -p 15441:15441 -p 43110:43110 binerf/zeronet_tor --restart=always
