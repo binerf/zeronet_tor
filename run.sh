d3f4da173e07e8a2ae5aa68d9ecfa6026831631a
Binerf <binerf@mygaia.org>
2018-11-12 21:54:30 +0100

    Fix issue in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#! /bin/bash -ex
+#!/bin/bash -ex
 
 /usr/bin/tor -f /usr/share/tor/tor-service-defaults-torrc 2>&1 &
 
-cd /home/zeronet/ZeroNet && sudo -u zeronet python zeronet.py --ui_ip 0.0.0.0 --config_file /home/zeronet/ZeroNet/data/zeronet.conf
+cd /home/zeronet/ZeroNet && sudo -u zeronet python zeronet.py --ui_ip '*' --config_file /home/zeronet/ZeroNet/data/zeronet.conf
