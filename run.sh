1fa5c1ba777f199a09b8d20d5bafbc6f0368f470
Binerf <binerf@mygaia.org>
2018-12-26 13:49:49 +0100

    Update size limit to 50M

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 /usr/bin/tor -f /usr/share/tor/tor-service-defaults-torrc 2>&1 &
 
-cd /home/zeronet/ZeroNet && sudo -u zeronet python zeronet.py --ui_ip '*' --config_file /home/zeronet/ZeroNet/data/zeronet.conf
+cd /home/zeronet/ZeroNet && sudo -u zeronet python zeronet.py --size_limit 50 --ui_ip '*' --config_file /home/zeronet/ZeroNet/data/zeronet.conf
